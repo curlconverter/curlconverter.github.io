5243600a306830c781ff96d1e8f06acfa9a88a2d
Nick Carneiro <nickc@indeed.com>
2015-07-03 08:54:05 -0700

    added new google analytics id

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,3 @@
-/**
- * scripts/main.js
- *
- * This is the starting point for your application.
- * Take a look at http://browserify.org/ for more info
- */
-
 'use strict';
 
 var curlconverter = require('curlconverter');
