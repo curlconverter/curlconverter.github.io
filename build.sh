2b7cf8249c88a10828c173ffe9682b3aee76d23c
Nick Carneiro <nickc@trillworks.com>
2020-06-17 20:00:09 +0000

    uglify does not support es6 so we will stop minifying for now

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-./node_modules/browserify/bin/cmd.js scripts/main.js | ./node_modules/uglify-es/bin/uglifyjs -o scripts/main.min.js
-
+/node_modules/browserify/bin/cmd.js scripts/main.js > scripts/main.min.js
