1f672de50f7f1260fcd93f72f317181d28a2271c
Boris Verkhovskiy <boris.verk@gmail.com>
2023-02-22 02:52:57 +0100

    Add languages from httpsnippet

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ import { fileURLToPath } from 'url'
 import CopyPlugin from 'copy-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
+import webpack from 'webpack'
+
 import hljs from 'highlight.js'
 
-import * as curlconverter from 'curlconverter'
+import { languages } from './languages.js'
 
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
@@ -16,30 +18,6 @@ const __dirname = path.dirname(__filename)
 const STARTING_CODE = `import requests
 
 response = requests.get('http://example.com')`
-
-const languages = {
-  ansible: { converter: curlconverter.toAnsibleWarn, hljs: 'yaml', title: 'Ansible' },
-  // TODO: CFML isn't supported by highlight.js
-  cfml: { converter: curlconverter.toCFMLWarn, hljs: 'javascript', title: 'ColdFusion Markup Language' },
-  csharp: { converter: curlconverter.toCSharpWarn, hljs: 'csharp', title: 'C#' },
-  dart: { converter: curlconverter.toDartWarn, hljs: 'dart', title: 'Dart' },
-  elixir: { converter: curlconverter.toElixirWarn, hljs: 'elixir', title: 'Elixir' },
-  go: { converter: curlconverter.toGoWarn, hljs: 'go', title: 'Go' },
-  java: { converter: curlconverter.toJavaWarn, hljs: 'java', title: 'Java' },
-  javascript: { converter: curlconverter.toJavaScriptWarn, hljs: 'javascript', title: 'JavaScript' },
-  // People googling for "curl json" are probably looking for something else
-  json: { converter: curlconverter.toJsonStringWarn, hljs: 'json', title: 'a JSON object' },
-  matlab: { converter: curlconverter.toMATLABWarn, hljs: 'matlab', title: 'MATLAB' },
-  'node-fetch': { converter: curlconverter.toNodeWarn, hljs: 'javascript', title: 'node-fetch' },
-  'node-axios': { converter: curlconverter.toNodeAxiosWarn, hljs: 'javascript', title: 'Node (Axios)' },
-  'node-got': { converter: curlconverter.toNodeGotWarn, hljs: 'javascript', title: 'Node (Got)' },
-  'node-request': { converter: curlconverter.toNodeRequestWarn, hljs: 'javascript', title: 'Node (request)' },
-  php: { converter: curlconverter.toPhpWarn, hljs: 'php', title: 'PHP' },
-  python: { converter: curlconverter.toPythonWarn, hljs: 'python', title: 'Python' },
-  r: { converter: curlconverter.toRWarn, hljs: 'r', title: 'R' },
-  ruby: { converter: curlconverter.toRubyWarn, hljs: 'ruby', title: 'Ruby' },
-  rust: { converter: curlconverter.toRustWarn, hljs: 'rust', title: 'Rust' },
-}
 
 const toLanguage = (language, title, converter, hljsLang) => {
   return (content) => {
@@ -85,11 +63,11 @@ export default {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        enforce: 'pre',
-        use: ['source-map-loader']
-      },
+      // {
+      //   test: /\.js$/,
+      //   enforce: 'pre',
+      //   // use: ['source-map-loader']
+      // },
       {
         test: /\.sh$/,
         type: 'asset/source'
@@ -117,7 +95,13 @@ export default {
   resolve: {
     fallback: {
       path: require.resolve('path-browserify'),
-      fs: false
+      fs: false,
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "url": require.resolve("url/"),
+      "querystring": require.resolve("querystring-es3"),
+      "string_decoder": require.resolve("string_decoder/"),
+      "buffer": require.resolve("buffer/")
     }
   },
   experiments: {
@@ -134,6 +118,9 @@ export default {
         'meta',
         'CNAME'
       ].concat(copyIndexHtml())
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser"
     })
   ],
   // Don't warn that we have a big JS bundle.
